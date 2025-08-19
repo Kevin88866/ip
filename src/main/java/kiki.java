05775929debbee81a82ad635bf332593c62a3075
Zheng_Kaiwen <zkw2542248162@gmail.com>
2025-08-19 16:31:04 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/kiki.java
@@ -0,0 +1,11 @@
+public class kiki {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
+        System.out.println(" Hello! I'm kiki");
+        System.out.println(" What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
