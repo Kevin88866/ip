63bacb95ceaf08441b07383c535cf9ee11d57b61
Zheng_Kaiwen <zkw2542248162@gmail.com>
2025-08-26 14:53:13 +0800

    feat: implement echo function and refactor message printing
    - implement echo loop that repeats user input until "bye"
    - Extracted message printing into sendMessage() for better code reuse

--- src/main/java/kiki.java
+++ src/main/java/kiki.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
 public class kiki {
-    public static void main(String[] args) {
+    private static void printMessage(String message){
         String horizontalLine = "____________________________________________________________";
         System.out.println(horizontalLine);
-        System.out.println(" Hello! I'm kiki");
-        System.out.println(" What can I do for you?");
+        System.out.println(" " + message);
         System.out.println(horizontalLine);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        printMessage(" Hello! I'm kiki\n  What can I do for you?");
+        String input;
+        while(!(input = sc.nextLine()).equals("bye")){
+            printMessage(input);
+        }
+        printMessage(" Bye. Hope to see you again soon!");
     }
 }
