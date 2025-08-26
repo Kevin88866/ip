bd97f8dc16e5b66becba900bd0d72fc7ac083735
Zheng_Kaiwen <zkw2542248162@gmail.com>
2025-08-26 15:57:59 +0800

    feat: add add,list funtionality

--- src/main/java/kiki.java
+++ src/main/java/kiki.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
 public class kiki {
+    private static final String horizontalLine = "____________________________________________________________";
+    private static String[] tasks = new String[100];
+    private static int count = 0;
+
     private static void MessagePrinter(String message){
-        String horizontalLine = "____________________________________________________________";
+
         System.out.println(horizontalLine);
         System.out.println(" " + message);
         System.out.println(horizontalLine);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MessagePrinter(" Hello! I'm kiki\n  What can I do for you?");
         String input;
         while(!(input = sc.nextLine()).equals("bye")){
-            MessagePrinter(input);
+            if(input.equals("list")){
+                System.out.println(horizontalLine);
+                for(int i = 0; i < count; i++) System.out.println((i + 1) + ". " + tasks[i]);
+                System.out.println(horizontalLine);
+            }else{
+                tasks[count] = input;
+                count++;
+                MessagePrinter(" added: " + input);
+            }
         }
         MessagePrinter(" Bye. Hope to see you again soon!");
+
         sc.close();
     }
 }
